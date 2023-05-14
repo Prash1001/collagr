8f5ccfb0f13e3362ca6575f7710cb9b1ba45842f
Prashant Patil <prashant1001001@gmail.com>
2023-05-14 18:54:45 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,14 @@
+let a=1
+let b=2
+
+c=a+b
+
+console.log(c)
+add("3","2");
+function add(a,b){
+console.log(a-b); 
+}
+
+let ar=[1,2,3,3,4,5];
+ar=ar.concat(3);
+console.log(ar);
---
+++ prahs.py
@@ -0,0 +1,1 @@
+print("prash is best")
